78993dce0bedd020bac400af656765df21a21606
DaseinUXD <markmatthewsphd@gmail.com>
2018-04-26 13:17:26 -0500

    Updating ColorClicker

--- DojoAssignments/WebFundamentals/Advanced jQuery/ColorClicker/script.js
+++ DojoAssignments/WebFundamentals/Advanced jQuery/ColorClicker/script.js
@@ -1,24 +1,31 @@
 "use strict";
 
+
 function random_color() {
     var rgb = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    color = '#'  //this is what we'll return!
+    var color = '#';  //this is what we'll return!
     for (var i = 0; i < 6; i++) {
-        x = Math.floor((Math.random() * 16))
+        var x = Math.floor((Math.random() * 16));
         color += rgb[x];
+
     }
     return color;
+    console.log(color);
 }
-
 $(document).ready(function () {
+
+    random_color();
+
+    console.log(color);
+    console.log('ready steady');
     $('#large_box').click(function () {
         alert('you clicked the big box!');
         //comment this out when you have figured out what event.stopPropagation is used for
-    })
+    });
     $('.side_box').click(function (event) {
         event.stopPropagation();
-    })
+    });
     $('.middle_box').click(function (event) {
         event.stopPropagation();
-    })
+    });
 });
