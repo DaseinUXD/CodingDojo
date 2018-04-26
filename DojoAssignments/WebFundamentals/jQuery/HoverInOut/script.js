cbd777a9a49ab75433484efa389dd1ac7ae19407
DaseinUXD <markmatthewsphd@gmail.com>
2018-04-25 20:17:45 -0500

    completed the Hover In/Out project

--- DojoAssignments/WebFundamentals/jQuery/HoverInOut/script.js
+++ DojoAssignments/WebFundamentals/jQuery/HoverInOut/script.js
@@ -1,5 +1,22 @@
 "use strict";
 
 $(document).ready(function(){
+    console.log('ready steady');
+    // $('#images').on('click', 'img', function () {
+    //     var srcNew = $(this).attr('src-alt');
+    //     console.log(srcNew);
+    //     $(this).attr('src', srcNew);
+    // })
+    $('#images img').hover(function(){
+        var srcNew = $(this).attr('src-alt');
+        console.log('hovered in', srcNew);
+        var srcOrig = $(this).attr('src');
+        $(this).attr('src', srcNew);
+        $(this).attr('src-alt', srcOrig)
+    }, function(){
+        var srcOld = $(this).attr('src-alt');
+        console.log('hovered out', 'src-alt', srcOld)
+        $(this).attr('src', srcOld);
+    });
 
 });
