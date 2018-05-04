fa4deaddab007b9095dd69e5ff6696fe145de006
DaseinUXD <markmatthewsphd@gmail.com>
2018-05-03 20:49:51 -0500

    What's my name?

---
+++ DojoAssignments/Python/PythonAssignments/FlaskFundamentals/WhatMyName/WhatMyNameServer.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/process', methods = ['POST'])
+def process():
+    # print 'got post'
+    name = request.form['name']
+    # name = request.form['name']
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
