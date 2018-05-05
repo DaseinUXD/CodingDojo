bb2a334df14284fddc962bdc854e43394e6b49ba
DaseinUXD <markmatthewsphd@gmail.com>
2018-05-04 21:45:24 -0500

    Flask: Dojo Survey again...

--- DojoAssignments/Python/PythonAssignments/FlaskFundamentals/DojoSurvey/SurveyServer.py
+++ DojoAssignments/Python/PythonAssignments/FlaskFundamentals/DojoSurvey/SurveyServer.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = 'this is the secret key'
 
 
+# landing page
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/result', methods=['POST'])
+# results page
+@app.route('/result', methods=['GET','POST'])
 def result():
-    name = request.form['name']
-    location = request.form['location']
-    return redirect('result.html')
+    # name = session['name']
+
+    return render_template('result.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # return "Hello"
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comment'] = request.form['comment']
+
+    return redirect('/result')
 
 
 if __name__ == '__main__':
